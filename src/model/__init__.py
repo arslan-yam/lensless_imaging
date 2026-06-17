2ec8c83c3336b7236e711b3d7c2eb242ca448724
Arslan Yamaletdinov <armaiamaletdinov@edu.hse.ru>
2026-06-17 20:46:27 +0800

    rrdbnet

--- src/model/__init__.py
+++ src/model/__init__.py
@@ -1,11 +1,15 @@
 from src.model.baseline_model import BaselineModel
 from src.model.drunet import DRUNet
 from src.model.fista import FISTA
+from src.model.realesrgan import RealESRGAN
 from src.model.reconstructor import LenslessReconstructor
+from src.model.rrdbnet import RRDBNet
 
 __all__ = [
     "BaselineModel",
     "DRUNet",
     "FISTA",
+    "RealESRGAN",
+    "RRDBNet",
     "LenslessReconstructor",
 ]
---
+++ src/model/realesrgan.py
@@ -0,0 +1,23 @@
+import torch
+from torch import nn
+from torch.nn import functional as F
+from src.model.rrdbnet import RRDBNet
+WEIGHTS_URL = "https://github.com/xinntao/Real-ESRGAN/releases/download/v0.1.0/RealESRGAN_x4plus.pth"
+
+
+class RealESRGAN(nn.Module):
+    def __init__(self, pretrained=True, frozen=True):
+        super().__init__()
+        self.net = RRDBNet(num_in_ch=3, num_out_ch=3, num_feat=64, num_block=23, num_grow_ch=32)
+        if pretrained:
+            state = torch.hub.load_state_dict_from_url(WEIGHTS_URL, map_location="cpu")
+            self.net.load_state_dict(state.get("params_ema", state))
+        if frozen:
+            for p in self.net.parameters():
+                p.requires_grad_(False)
+
+    def forward(self, x):
+        h, w = x.shape[-2:]
+        x = x / (x.amax(dim=(-3, -2, -1), keepdim=True) + 1e-8)
+        out = self.net(x)
+        return F.interpolate(out, size=(h, w), mode="bilinear", align_corners=False)
